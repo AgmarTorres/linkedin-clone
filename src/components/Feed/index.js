8b88d7ac25eb5b5ddc2c5c01b7e42c639fd3f2e4
Agmar Torres <agmartorres@gmail.com>
2021-03-12 16:33:15 -0300

    feat: doing sidebar

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,8 +1,14 @@
-import { Create, Image } from '@material-ui/icons';
+import {
+  CalendarViewDay,
+  Create,
+  EventNote,
+  Image,
+  Subscriptions,
+} from '@material-ui/icons';
 import React from 'react';
 
-import './styles.css';
 import InputOptions from './../InputOptions/index';
+import './styles.css';
 
 function Feed() {
   return (
@@ -16,7 +22,14 @@ function Feed() {
           </form>
         </div>
         <div className='feed__inputOptions'>
-          <InputOptions title='' Icon={Image} color='#70B5F9' title='Photo' />
+          <InputOptions Icon={Image} color='#70B5F9' title='Photo' />
+          <InputOptions Icon={Subscriptions} color='#E7A33E' title='Video' />
+          <InputOptions Icon={EventNote} color='#C0CBCD' title='Event' />
+          <InputOptions
+            Icon={CalendarViewDay}
+            color='#7FC15E'
+            title='Write article'
+          />
         </div>
       </div>
     </div>
---
+++ src/components/Post/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Post({ name, description }) {
+  return <div className='post'></div>;
+}
+
+export default Post;
