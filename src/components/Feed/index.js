48aca56c4e7fd004231540f858b47340ae6aa95a
Agmar Torres <agmartorres@gmail.com>
2021-03-12 15:17:28 -0300

    first input with image in body

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,7 +1,26 @@
+import { Create, Image } from '@material-ui/icons';
 import React from 'react';
 
+import './styles.css';
+import InputOptions from './../InputOptions/index';
+
 function Feed() {
-  return <div></div>;
+  return (
+    <div className='feed'>
+      <div className='feed__inputContainer'>
+        <div className='feed__input'>
+          <Create />
+          <form>
+            <input type='text' />
+            <button type='submit'>Send</button>
+          </form>
+        </div>
+        <div className='feed__inputOptions'>
+          <InputOptions title='' Icon={Image} color='#70B5F9' title='Photo' />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
